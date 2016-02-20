60c06dde5008332b7b27c47d93ec0a452dc03d78
Qiyuan Qiu <qiuqiyuan@gmail.com>
2016-02-20 13:20:22 -0500

    Add hello_world exmple

---
+++ hello.js
@@ -0,0 +1,6 @@
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(8080, '10.132.251.159');
+console.log('Server running at http://10.132.251.159:8080/');
