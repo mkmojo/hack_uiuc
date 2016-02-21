379949b8be4433e760574c6a6bf6fc8935775ca2
Qiyuan Qiu <qiuqiyuan@gmail.com>
2016-02-21 03:08:17 -0500

    Add jobid and occu name mapping

--- server.js
+++ server.js
@@ -1,25 +1,53 @@
+var $ = require('jquery');
 var http = require('http');
 
-var options = {
-    host: 'api.indeed.com',
-    port: 80,
-    path: '/ads/apisearch?publisher=7699004885042305&q=java&l=austin%2C+tx&sort=&radius=&st=&jt=&start=&limit=300&fromage=&filter=1&latlong=1&co=us&format=json&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2'
-};
+//returns
+function getInfoFromIndeed(occu) {
+    var options = {
+        host: 'api.indeed.com',
+        port: 80,
+        path: '/ads/apisearch?publisher=7699004885042305&q='+ occu + '&l=austin%2C+tx&sort=&radius=&st=&jt=&start=&limit=3000000&fromage=&filter=1&latlong=1&co=us&format=json&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2'
+    };
 
-http.get(options, function(resp){
-    var res = "";
-    resp.setEncoding('utf8');
-    resp.on('data', function(chunk){
-        res += chunk;
+    http.get(options, function(resp){
+        var res = "";
+        resp.setEncoding('utf8');
+        resp.on('data', function(chunk){
+            res += chunk;
+        }).on('end', function() {
+            res = JSON.parse(res);
+            //console.dir(res);
+        });
+    }).on("error", function(e){
+        console.log("Got error: " + e.message);
     });
-    resp.on('end', function() {
-        var test = JSON.parse(res);
-        console.dir(test);
-        console.log("DEBUG: Parsed!!");
-    });
-}).on("error", function(e){
-    console.log("Got error: " + e.message);
-});
+}
+
+var jobid_2_occr = require('./occupations/occpuations.json');
+
+console.log(jobid_2_occr["9110"]);
+
+var states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY', 'DC', 'AS', 'GU', 'MP', 'PR', 'UM', 'VI'];
+
+//number of potential spouses in each state
+var counts = {};
+
+var indeed_responses = []
+for(job_id in hash) {
+    if(hash.hasOwnProperty(job_id)) {
+        getInfoFromIndeed(hash(job_id))
+    }
+}
+
+function make_top10_list(job_id) {
+    top10_list[job_id];
+}
+
+function make_counts(job_id) {
+    var cnt_state = entry['state'];
+    counts[cnt_state] += 1;
+    return counts;
+}
 
 
 
